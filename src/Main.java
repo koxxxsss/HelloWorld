a6c2002e9d8334e2dd1bb1a885981159c8ee74d7
koxxx <koxxxsss@ya.ru>
2023-11-06 21:42:39 +0300

    поменяли надпись

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,8 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("77777777777777777777");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        System.out.printf("Hello777777777777welcome!");
+    // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
